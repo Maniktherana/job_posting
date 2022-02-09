5ac3e815ecd485444f956f10f9b69e7d3e735bc4
Manik Rana <manikrana54@gmail.com>
2022-02-09 13:26:34 -0500

    Initial commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
