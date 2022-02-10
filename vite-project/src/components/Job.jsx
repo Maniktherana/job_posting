b9435b575d1fb189cfa745953bc7c52e32254d0a
Manik Rana <manikrana54@gmail.com>
2022-02-09 22:39:54 -0500

    Add cards

--- vite-project/src/components/Job.jsx
+++ vite-project/src/components/Job.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Card, Badge, Button } from 'react-bootstrap'
 
 export default function Job({ job }) {
   return (
-    <div>
-        {job.name}
-    </div>
+    <Card>
+        <div className="d-flex justify-content-between">
+            <div>
+                <Card.Title>
+                    {job.name} - <span className="text-muted font-weight-light">{job.company}</span>
+                </Card.Title>
+                <Card.Subtitle>
+                    {job.lastUpdated}
+                </Card.Subtitle>
+                {job.role}
+                <Badge>{job.level}</Badge>
+                <Card.Text>
+                    <a href={job.url} target="_blank">
+                    <Button
+                        variant="primary"
+                    >
+                        Visit site
+                    </Button>
+                    </a>
+                </Card.Text>
+            </div>
+        </div>
+    </Card>
   )
 }
