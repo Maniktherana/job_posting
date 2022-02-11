f1921d4067bc7f2b36d2bb5cfa2c10d2be5a0d37
Manik Rana <manikrana54@gmail.com>
2022-02-10 20:03:39 -0500

    Add Pagination component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import { Navbar, Welcome, Footer, Listings, Job } from './components'
+import { Navbar, Welcome, Footer, Listings, Job, JobsPagination } from './components'
 import useFetchJobs from './useFetchJobs'
 import { Container } from 'react-bootstrap'
 
@@ -9,7 +9,7 @@ function App() {
   const [count, setCount] = useState(0)
   const [params, setParams] = useState({})
   const [page, setPage] = useState(1)
-  const { jobs, loading, error} = useFetchJobs(params, page)
+  const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page)
 
   return (
     <div className="min-h-screen">
@@ -18,11 +18,13 @@ function App() {
         <Welcome />
       </div>
       <Container>
+        <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage}/>
         {loading && <h1>Loading...</h1>}
         {error && <h1>Error</h1>}
         {jobs.map(job => {
           return <Job key={job.id} job={job} />
         })}
+        <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage}/>
       </Container>
       <Listings />
       <Footer />
--- vite-project/src/useFetchJobs.js
+++ vite-project/src/useFetchJobs.js
@@ -4,7 +4,8 @@ import axios from 'axios'
 const ACTIONS = {
     MAKE_REQUEST: 'makeRequest',
     GET_DATA: 'getData',
-    ERROR: 'error'
+    ERROR: 'error',
+    UPDATE_HAS_NEXT_PAGE: "update-has-next-page"
 }
 
 const BASE_URL = "http://localhost:3000/listings"
@@ -39,6 +40,8 @@ function reducer(state, action) {
             return { ...state, loading: false, jobs: action.payload.jobs}
         case ACTIONS.ERROR:
             return { ...state, loading: false, error: action.payload.error, jobs:[] }
+        case ACTIONS.UPDATE_HAS_NEXT_PAGE:
+            return {...state, hasNextPage: action.payload.hasNextPage}
         default:
             return state
     }
@@ -48,10 +51,11 @@ export default function useFetchJobs(params, page) {
     const [state, dispatch] = useReducer(reducer, {jobs: [], loading: true})
 
     useEffect(() => {
-        const cancelToken = axios.CancelToken.source()
+        const cancelToken1 = axios.CancelToken.source()
         dispatch({ type: ACTIONS.MAKE_REQUEST})
+
         axios.get(currentUrl, {
-            cancelToken: cancelToken.token,
+            cancelToken: cancelToken1.token,
             params: { page: page, ...params }
         }).then(res => {
             dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data } })
@@ -60,8 +64,21 @@ export default function useFetchJobs(params, page) {
             dispatch({ type: ACTIONS.ERROR, payload: { error: e }})
         })
 
+        const cancelToken2 = axios.CancelToken.source()
+
+        axios.get(currentUrl, {
+            cancelToken: cancelToken2.token,
+            params: { page: page + 1, ...params }
+        }).then(res => {
+            dispatch({ type: ACTIONS.UPDATE_HAS_NEXT_PAGE, payload: { hasNextPage: res.data.length !== 0 } })
+        }).catch(e => {
+            if (axios.isCancel(e)) return
+            dispatch({ type: ACTIONS.ERROR, payload: { error: e }})
+        }) 
+
         return () => {
-            cancelToken.cancel()
+            cancelToken1.cancel()
+            cancelToken2.cancel()
         }
     }, [params, page])
 
