1fdb5fda3cc119c5436a135f0c6901f958a72827
Manik Rana <manikrana54@gmail.com>
2022-02-16 16:46:53 -0500

    Update Nav links

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { HiMenuAlt4 } from "react-icons/hi"
 import { AiOutlineClose } from "react-icons/ai"
-import { NextUIProvider, createTheme } from '@nextui-org/react'
+import { NextUIProvider, createTheme, Link } from '@nextui-org/react'
 
 const darkTheme = createTheme({
     type: "dark",
@@ -27,27 +27,11 @@ const Navbar = () => {
                         <h3 className="w-32 cursor-pointer">Job Site</h3>
                     </div>
                     <ul className="md:flex hidden list-none flex-row justify-between items-center flex-initial">
-                        {["About", "Contact", "Jobs"].map((item, index) => (
-                        <NavBarItem key={item + index} title={item} />
-                        ))}
+                        <li className={`mx-4 cursor-pointer`}>
+                            <Link href="https://www.nowhiteboard.org/" target="_blank" icon color="text">No Whiteboard</Link>
+                        </li>
                     </ul>
-                    <div className="flex relative">
-                        {!toggleMenu && (
-                        <HiMenuAlt4 fontSize={28} className="md:hidden cursor-pointer" onClick={() => setToggleMenu(true)} />
-                        )}
-                        {toggleMenu && (
-                        <AiOutlineClose fontSize={28} className="md:hidden cursor-pointer z-20" onClick={() => setToggleMenu(false)} />
-                        )}
-                        {toggleMenu && (
-                        <ul className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
-                            flex flex-col justify-start items-center rounded-md blue-glassmorphism text-black animate-slide-in">
-                                {["About", "Contact", "Jobs"].map((item, index) => 
-                                <NavBarItem key={item + index} title={item} classprops="my-2 text-lg" />,
-                                )}
-                            
-                        </ul>
-                        )}
-                    </div>
+                    
                 </div>
             </nav>
         </NextUIProvider>
