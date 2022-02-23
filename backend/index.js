5c477cbb76db8bbbb5727e13a71957dda3f60e3c
Manik Rana <manikrana54@gmail.com>
2022-02-23 16:49:18 -0500

    Add procfile

--- backend/index.js
+++ backend/index.js
@@ -135,7 +135,7 @@ let jobs = [
   
   app.use(unknownEndpoint)
 
-  const PORT = process.env.PORT
+  const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
