75d997754049c64f853dc8db138f786860a087a5
Manik Rana <manikrana54@gmail.com>
2022-09-09 15:35:40 +0530

    Add v3

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import Hero from "./components/Hero";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <h1>GET YOUR DREAM JOB</h1>
+      <p>FIND TECH JOBS WITHOUT THE NIGHTMARE OF BAD INTERVIEWS</p>
+    </div>
+  );
+};
+
+export default Hero;
